a29186c40ab4f079be0e59d16b3c0e0807eb4350
irreplaceable <33859704+niphoenixo@users.noreply.github.com>
2019-10-15 14:04:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {Text, View} from 'react-native-web';
+//import Login from "./components/Login.js";
+
+
+/*function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}*/
+
+const App:React.FC = () =>{
+  return (
+      <View>
+       <Text>Hi Buddy, Welcome to react native world!!</Text>
+      </View>
+  );
+}
+
+export default App;
