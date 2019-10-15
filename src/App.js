a92289d8699062b914104419bcb63b1b5af5c025
irreplaceable <33859704+niphoenixo@users.noreply.github.com>
2019-10-15 14:48:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import * as React from 'react';
 import {Text, View} from 'react-native-web';
-//import Login from "./components/Login.js";
-
-
-/*function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
-}*/
 
 const App:React.FC = () =>{
   return (
